8f2df0feb85852ea6b839abe35a34317146a7fa1
praveenkumar-1998 <tpk6123@gmail.com>
2026-07-06 01:35:22 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chatbot-portfolio/' // repo name
+  // base: '/chatbot-portfolio/' // repo name
+  base: '/ai-chatbot-react/'
 })
